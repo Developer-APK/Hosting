fef0c2365006aa94bc7dac4eaf90a44238c5fb76
Developer-APK <apk93149@gmail.com>
2024-04-18 18:27:11 +0530

    initiali_code pushed

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const router = require("./routes/mainRoutes");
+const cors = require("cors");
+const port = process.env.PORT;
+app.use(express.json());
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type"],
+    })
+);
+
+app.use("/data", router);
+app.get("/", (req, res) => {
+    res.send("i'm live");
+});
+app.listen(port, () => {
+    console.log(`server is live at ${port}`);
+});
+
+module.exports = app;
