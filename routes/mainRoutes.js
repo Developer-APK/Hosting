3a5b8e4f088dbfb54721a1b5dfd1961b5040d413
Archana.S.M <141334291+Developer-APK@users.noreply.github.com>
2024-04-22 14:51:21 +0530

    Update mainRoutes.js

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const getData = require("../controllers/getData");
 const insertData = require("../controllers/insertData");
-router.get("/", getData);
+router.post("/", getData);
 router.post("/insert", insertData);
 module.exports = router;
